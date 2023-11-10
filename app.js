2226a6b78491570c7bfc5e0dbfba8c3018d0ed07
Kayla Dale <kmeyers119@gmail.com>
2023-11-10 13:15:28 -0600

    added conditionals to functions

--- app.js
+++ app.js
@@ -1,52 +1,26 @@
-// let namE = document.getElementById('name');
-
-//let email = document.getElementById('email');
-
-//let numb = document.getElementById('numb');
-
-//let msg = document.getElementById('msg');
-
-//let form = document.getElementById('form');
-
-//let nameError = document.getElementById("name-error");
-
-//let emailError = document.getElementById("email-error");
-
-//let numbError = document.getElementById("numb-error");
-
-//let msgError = document.getElementById("msg-error");
-
-//let submitError = document.getElementById("submit-error");
-
-
-function error() {
-    document.getElementById('name-error').style.visibility = "hidden";
-    document.getElementById('email-error').style.visibility = "hidden";
-    document.getElementById('numb-error').style.visibility = "hidden";
-    document.getElementById('msg-error').style.visibility = "hidden";
-}
-
 function checkName() {
     let name = document.getElementById('name').value; // length or value???
-    if (name.length == ''){
+    if (name.length === 0){
     document.getElementById('name-error').style.visibility = "visible";
 } else {
     document.getElementById('name-error').style.visibility = "hidden";
 }
 
-//if(!name.match(/^[A-Za-Z]+$/)) {
-//    document.getElementById('name-error').style.visibility = "visible";
-//}
+if(!name.match(/^[A-Za-z ]+$/)) {
+    document.getElementById('name-error').style.visibility = "visible";
+}
 }
 
 
 function checkEmail() {
     let email = document.getElementById('email').value;
-    if (email.length == '') {
+    if (email.length === 0) {
     document.getElementById('email-error').style.visibility = 'visible';
 } else {
     document.getElementById('email-error').style.visibility = 'hidden';
-}}
+}
+}
+
 
 function checkNumb() {
     let numb = document.getElementById('numb').value;
